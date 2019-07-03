634aa51ba209312facd8271af42fae529d95e867
roy <sajezz@gmail.com>
2019-07-03 16:29:51 +0300

    adding nodemon

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -42,5 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3001, () => console.log('yay!'));
 
 module.exports = app;
