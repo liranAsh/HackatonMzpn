4115f79eedaffcca7f6905f9a0576c9185af0c0a
Amit Hanoch <amit.dvirhanoch@gmail.com>
2019-07-03 15:59:06 +0300

    add cards and material-ui

--- client/src/guardsToday/GuardsTodayBox.jsx
+++ client/src/guardsToday/GuardsTodayBox.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import '../App.css';
 import GuardsList from './GuardsList';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 class GuardsToday extends Component {
-  
+
   render() {
     return (
-        <div className="guardstodaybox">
-          <h2>שומרים היום</h2>
+        <Paper style={styles.card}>
+          <Typography variant="h5" component="h3" style={styles.cardTitle}>
+            שומרים היום
+          </Typography>
           <GuardsList headline="יורדים"></GuardsList>
           <GuardsList headline="עולים"></GuardsList>
-        </div>
+        </Paper>
     );
   }
 }
+
+const styles = 
+{
+  card: {
+    margin: '0 auto',
+    padding: 16,
+    borderRadius: 16,
+    width: '50%',
+    height: 'auto'
+  },
+  cardTitle: {
+  }
+};
+
 export default GuardsToday;
